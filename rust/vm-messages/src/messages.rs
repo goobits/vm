27e209fcc0ea034f7b270c6240071e88328dfd8e
Miko Meow <101564+mudcube@users.noreply.github.com>
2025-10-01 22:37:40 +0000

    Revert "refactor: split Messages struct into declaration and const instance"
    This reverts commit b59894176806327d9016dc710049a2ab2bc62b64.

--- rust/vm-messages/src/messages.rs
+++ rust/vm-messages/src/messages.rs
@@ -1,8 +1,8 @@
-pub struct Messages {
+pub const MESSAGES: Messages = Messages {
     // ... existing fields
     pub vm_service_cleanup_success: &'static str,
     // ... rest of the fields
-}
+};
 
 pub const MESSAGES: Messages = Messages {
     // ... existing values
